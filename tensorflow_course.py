8ee3653f0490124cd8dc3e80e7da73268fedceac
Beatriz Nishi <beatriz.nishi@gmail.com>
2020-09-04 19:01:13 -0300

    complete

--- tensorflow_course.py
+++ tensorflow_course.py
@@ -20,3 +20,62 @@ def dummy_creation(df, dummy_categories):
     return(df)
 
 df = dummy_creation(df, ['Egg_Group_1', 'Body_Style', 'Color','Type_1', 'Type_2'])
+
+def train_test_splitter(DataFrame, column):
+    """This function takes any Pokémon whose 'Generation' label is equal to 1 and putting 
+    it into the test dataset, and putting everyone else in the training dataset. 
+    It then drops the Generation category from the dataset"""
+
+    df_train = DataFrame.loc[df[column] != 1]
+    df_test = DataFrame.loc[df[column] == 1]
+
+    df_train = df_train.drop(column, axis=1)
+    df_test = df_test.drop(column, axis=1)
+
+    return(df_train, df_test)
+
+df_train, df_test = train_test_splitter(df, 'Generation')
+
+def label_delineator(df_train, df_test, label):
+    """This function extracts the data from the 
+    DataFrame and puts it into arrays that TensorFlow can understand with.values"""
+    
+    train_data = df_train.drop(label, axis=1).values
+    train_labels = df_train[label].values
+    test_data = df_test.drop(label,axis=1).values
+    test_labels = df_test[label].values
+    return(train_data, train_labels, test_data, test_labels)
+
+train_data, train_labels, test_data, test_labels = label_delineator(df_train, df_test, 'isLegendary')
+
+def data_normalizer(train_data, test_data):
+    """normalize the data so everything is on the same scale"""
+    train_data = preprocessing.MinMaxScaler().fit_transform(train_data)
+    test_data = preprocessing.MinMaxScaler().fit_transform(test_data)
+    return(train_data, test_data)
+
+train_data, test_data = data_normalizer(train_data, test_data)
+
+length = train_data.shape[1]
+
+model = keras.Sequential()
+model.add(keras.layers.Dense(500, activation='relu', input_shape=[length,]))
+model.add(keras.layers.Dense(2, activation='softmax'))
+
+model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(train_data, train_labels, epochs=400)
+
+loss_value, accuracy_value = model.evaluate(test_data, test_labels)
+print(f'Our test accuracy was {accuracy_value}')
+
+def predictor(test_data, test_labels, index):
+    prediction = model.predict(test_data)
+    if np.argmax(prediction[index]) == test_labels[index]:
+        print(f'This was correctly predicted to be a \"{test_labels[index]}\"!')
+    else:
+        print(f'This was incorrectly predicted to be a \"{np.argmax(prediction[index])}\". It was actually a \"{test_labels[index]}\".')
+        return(prediction)
+
+#Let's test our model using Mewtwo. He's number 150 in the list of Pokémon, so we'll look at index 149
+predictor(test_data, test_labels, 149)
